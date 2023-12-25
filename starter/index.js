666ed63f6a8f166d598f2bf460406f78dc3aaabf
udani521 <udani.karunratne@gmail.com>
2023-12-25 16:44:55 +0000

    updated readme file and added image of console screenshot

--- starter/index.js
+++ starter/index.js
@@ -1,4 +1,4 @@
-var finances = [
+const finances = [
   ['Jan-2010', 867884],
   ['Feb-2010', 984655],
   ['Mar-2010', 322013],
@@ -86,9 +86,12 @@ var finances = [
   ['Jan-2017', 138230],
   ['Feb-2017', 671099],
 ];
+
+
 console.log("Financial Analysis");
 console.log("-------------------");
 
+console.table(finances);
 
 // Initialize variables
 let totalMonths = finances.length;       // toatal number of months.
